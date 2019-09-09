d395a7493df07b3863ef2a15fae689291ac9a435
Code <Code@192.168.100.2>
2019-09-09 18:34:13 +0530

    last commit

--- mvnjavaselenium/src/main/java/mvnjavaseleniumpkg/Utility.java
+++ mvnjavaselenium/src/main/java/mvnjavaseleniumpkg/Utility.java
@@ -7,42 +7,45 @@ import java.io.IOException;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
 
-
-
-public class Utility 
-{
+public class Utility {
 	static String data = null;
 
-	public static void getDataFromExcel(String filepath, String sheetName, int row, int cell) throws IOException 
-	{
+	public static void getDataFromExcel(String filepath, String sheetName, int row, int cell) throws IOException {
 
-		File f = new File(filepath);					//create object for File class and pass the file path
-		FileInputStream fis = new FileInputStream(f);	//create File input stream class object and pass the file ref
-		Workbook wb = WorkbookFactory.create(fis);		//covert regular file to workbook type
-		Sheet st = wb.getSheet(sheetName);				//get sheet by workbook	
-		Row r = st.getRow(row);							//get row by sheet
-		Cell c = r.getCell(cell);						//get cell by row
-		data = c.toString();							// convert cell value to string type
-		//System.out.println(data);										
+		File f = new File(filepath); // create object for File class and pass the file path
+		FileInputStream fis = new FileInputStream(f); // create File input stream class object and pass the file ref
+		Workbook wb = WorkbookFactory.create(fis); // covert regular file to workbook type
+		Sheet st = wb.getSheet(sheetName); // get sheet by workbook
+		Row r = st.getRow(row); // get row by sheet
+		Cell c = r.getCell(cell); // get cell by row
+		data = c.toString(); // convert cell value to string type
+		// System.out.println(data);
 	}
 
+	public static void setDataToExcel(String filepath, String sheetName, int row, int cell, String data)
+			throws EncryptedDocumentException, IOException {
 
+		File f = new File(filepath);
+		FileInputStream fis = new FileInputStream(f);
+		Workbook wb = WorkbookFactory.create(fis);
+		Sheet st = wb.getSheet(sheetName);
+		Row r = st.getRow(row);
+		Cell c = r.getCell(cell);
+		c.setCellValue(data);
+		FileOutputStream fos = new FileOutputStream(f);
+		wb.write(fos);
 
+	}
 
-public static void setDataToExcel(String filepath, String sheetName, int row, int cell,String data) throws EncryptedDocumentException, IOException 
-{
+	public static void screenShot(WebDriver driver) throws Exception {
 
-	File f = new File(filepath);
-	FileInputStream fis = new FileInputStream(f);
-	Workbook wb = WorkbookFactory.create(fis);
-	Sheet st = wb.getSheet(sheetName);
-	Row r = st.getRow(row);
-	Cell c = r.getCell(cell);
-	c.setCellValue(data);
-	FileOutputStream fos = new FileOutputStream(f);
-	wb.write(fos);
-
-}
-
+		File src= ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src, new File("C:/selenium/" + System.currentTimeMillis() + ".png"));
+		//FileUtils.copyFile(src, new File("C:/selenium/" + System.currentTimeMillis() + ".png")); OLDCODE
+	}
 }
